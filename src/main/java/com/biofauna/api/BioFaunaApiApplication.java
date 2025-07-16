db863dcee97013d50692968f8a8915478756ea6d
Ulises Guzman <guzmangimenezulises06@gmail.com>
2025-07-16 20:40:47 -0300

    Init Spring Boot backend BioFaunaAPI

---
+++ src/main/java/com/biofauna/api/BioFaunaApiApplication.java
@@ -0,0 +1,13 @@
+package com.biofauna.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioFaunaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioFaunaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biofauna/api/BioFaunaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biofauna.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioFaunaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
